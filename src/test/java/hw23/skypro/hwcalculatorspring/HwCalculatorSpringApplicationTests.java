c19695928d73c9d6060a6db693c85bd71285167f
Tsvetkova Nina <tsvnishka@gmail.com>
2023-03-20 17:28:43 +0300

    Create project HW_calculator_spring

---
+++ src/test/java/hw23/skypro/hwcalculatorspring/HwCalculatorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hw23.skypro.hwcalculatorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCalculatorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
