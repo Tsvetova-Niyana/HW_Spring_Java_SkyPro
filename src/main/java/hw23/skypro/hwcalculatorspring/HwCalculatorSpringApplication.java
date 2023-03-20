e6687f87158c612ced793e80821833c6d89a94ae
Tsvetkova Nina <tsvnishka@gmail.com>
2023-03-20 18:37:28 +0300

    Create project HW_calculator_spring

---
+++ src/main/java/hw23/skypro/hwcalculatorspring/HwCalculatorSpringApplication.java
@@ -0,0 +1,13 @@
+package hw23.skypro.hwcalculatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCalculatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwCalculatorSpringApplication.class, args);
+	}
+
+}
